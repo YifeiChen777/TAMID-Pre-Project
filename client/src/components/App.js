c92ccc7fd373f40cee1e3b5a324f012aec71463e
asherwillner <asherjwillner@gmail.com>
2020-08-27 11:03:05 -0700

    Asher testing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -48,6 +48,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>ASHER IS WATCHING</h1>
         <p>{this.state.message || 'No message'}</p>
       </div>
     );
