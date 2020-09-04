27f9c73b7f09ccc2bc96e6ba4528afb70bf89728
Caroline Chen <yc4ag@virginia.edu>
2020-09-04 17:32:08 -0400

    some fixes

--- client/src/components/submissions/submissions.js
+++ client/src/components/submissions/submissions.js
@@ -7,30 +7,29 @@ class Submissions extends Component {
 
     this.state = {
         results:{},
-        data:''
     };
   }
 
   handleSubmit = event => {
     event.preventDefault();
-    this.setState({data : this.data.value});
-    console.log(this.state.data);
-    this.fetchResult();
+    const body = {'text' : this.data.value};
+    console.log(body);
+    this.fetchResult(body);
   };
 
-  async fetchResult() {
-    const url = '/testpost'
+  async fetchResult(body) {
+    const url = '/testpost';
     const response = await fetch(url, {
         method: 'POST', // *GET, POST, PUT, DELETE, etc.
         mode: 'cors', // no-cors, *cors, same-origin
         cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
         credentials: 'same-origin', // include, *same-origin, omit
         headers: {
-            'Content-Type': 'text/plain'
+            'Content-Type': 'application/json'
         },
         redirect: 'follow', // manual, *follow, error
         referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: this.state.data // body data type must match "Content-Type" header
+        body: JSON.stringify(body) // body data type must match "Content-Type" header
         });
 
     this.setState({ results : response});
@@ -48,7 +47,7 @@ class Submissions extends Component {
          </div>
          <h2>Result</h2>
          <ul>
-         <li>{this.state && this.state.results.token}</li>
+         <li>{this.state.results && this.state.results.token}</li>
          <li>{this.state && this.state.results.stderr}</li>
          <li>{this.state && this.state.results.stdout}</li>
          <li>{this.state && this.state.results.compile_output}</li>
