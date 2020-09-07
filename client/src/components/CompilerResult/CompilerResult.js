f2315c6d5844e0a6acc3374c130e289102aa4bd0
talia krumerman <taliakrumerman@talias-MacBook-Pro.local>
2020-09-07 23:43:02 +0300

    shows error message

--- client/src/components/CompilerResult/CompilerResult.js
+++ client/src/components/CompilerResult/CompilerResult.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import  Paper  from '@material-ui/core/Paper';
 import './CompilerResult.scss'
 
-
-// Placeholder implementation of CodeEditor
 class CompilerResult extends Component {
 
     constructor(){
@@ -17,25 +15,37 @@ class CompilerResult extends Component {
         result = <Paper className='loading' elevation={5}>Loading...</Paper> 
       }
       else if (this.props.executionResult == null){
-        result = <Paper className='no-result'> The Output of your code will be displayed here </Paper>
+        result = 
+        <Paper className='no-result' style={{backgroundColor: '#ECECEC'}}>
+          <p style={{color: '#6A6A6A'}}>
+            The output of your code will be displayed here
+          </p>
+        </Paper>
       }
       else if (this.props.executionResult.compile_output) {
         if(this.props.executionResult.status.description){
           result = 
-          <Paper 
-            className="compiler-error" 
-            style={{backgroundColor: '#FCC2C2'}}>
-            <p style = {{color: '#900C3F'}}>
-              {this.props.executionResult.status.description}
-            </p>
-          </Paper>
+            <Paper 
+              className="compiler-error" 
+              style={{backgroundColor: '#FCC2C2'}}>
+              <p style = {{color: '#900C3F'}}>
+                {this.props.executionResult.status.description}
+              </p>
+              <p style = {{color: '#900C3F'}}>
+              {this.props.executionResult.compile_output}
+              </p>
+            </Paper>
         }
       }
       else{
         result = 
         <Paper className='compilerResult' elevation={3} style={{backgroundColor: '#ABEBC6'}}> 
-          <p style = {{color: '#0B5345'}}>Output: {this.props.executionResult.stdout}</p>
-          <p style ={{color: '#0B5345'}}>Status: {this.props.executionResult.status.description}</p>
+          <p style = {{color: '#0B5345'}}>
+            Output: {this.props.executionResult.stdout}
+          </p>
+          <p style ={{color: '#0B5345'}}>
+            Status: {this.props.executionResult.status.description}
+          </p>
         </Paper>
       }
       return (
